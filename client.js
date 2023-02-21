325acf1161d84e4b7d20bd8c3cc4b3d798554853
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-20 23:38:09 -0500

    Implemented export module

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  //We used Node's net library (specifically, the createConnection function) to create an object named conn in the code below.
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+  //event handler on data
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+  //event handler to a server and execute callback function
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server!");
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  //The conn object that Node returned to us represents the connection that you have with the server.
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,28 +1,5 @@
 //This will serve as our main file through which we will launch the game client.
-
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  //We used Node's net library (specifically, the createConnection function) to create an object named conn in the code below.
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-  //event handler on data
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  //event handler to a server and execute callback function
-  conn.on("connect", () => {
-    console.log("Successfully connected to the game server!");
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //The conn object that Node returned to us represents the connection that you have with the server.
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
