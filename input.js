17c1bbc27443e9b4ea3d7be8642a128242aca8e9
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-21 11:24:30 -0500

    Added wasd movements.

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 //the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +14,18 @@ const setupInput = function () {
     //check for (ctrl + c) command input
     if (key === "\u0003") {
       process.exit();
+    } else if (key === "w") {
+      // console.log("Move: up");
+      connection.write("Move: up");
+    } else if (key === "a") {
+      // console.log("Move: left");
+      connection.write("Move: left");
+    } else if (key === "s") {
+      // console.log("Move: down");
+      connection.write("Move: down");
+    } else if (key === "d") {
+      // console.log("Move: right");
+      connection.write("Move: right");
     }
   };
   stdin.on("data", handleUserInput);
