5d1e4f1ec9d1cbaf5d398a876214551aa74c2e5d
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-22 17:24:26 -0500

    Clean up the code by deleting some comments.

--- client.js
+++ client.js
@@ -3,10 +3,9 @@ const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
-  //We used Node's net library (specifically, the createConnection function) to create an object named conn in the code below.
   const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT, // PORT number here,
+    host: IP,
+    port: PORT,
   });
   //event handler on data
   conn.on("data", (data) => {
@@ -15,27 +14,11 @@ const connect = function () {
   //event handler to a server and execute callback function
   conn.on("connect", () => {
     console.log("Successfully connected to the game server!");
-    // send intial name prompt on connection
-    conn.write("Name: ES");
 
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 2000);
-    // setInterval(() => {
-    //   conn.write("Move: left");
-    // }, 3000);
-    // setInterval(() => {
-    //   conn.write("Move: down");
-    // }, 4000);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 5000);
+    conn.write("Name: ES"); // send intial name prompt on connection
   });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //The conn object that Node returned to us represents the connection that you have with the server.
-  return conn;
+  conn.setEncoding("utf8"); // interpret incoming data as text
+  return conn; //The conn object that Node returned to us represents the connection that you have with the server.
 };
 
 module.exports = { connect };
